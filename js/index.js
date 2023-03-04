447e3f77dc9269318cff3240bbc1d6dfcaa534e0
asifikbal09 <asif.ikbal662@gmail.com>
2023-03-04 16:39:59 +0600

    one side modal complite

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
 
 const loadAllAi = async (dataLimit) => {
     toggleLoader(true)
-    const url = ' https://openapi.programming-hero.com/api/ai/tools'
+    const url = 'https://openapi.programming-hero.com/api/ai/tools'
     const res = await fetch(url)
     const data = await res.json();
-    displayAllAi(data.data.tools, dataLimit)
+    displayAllAi(data.data.tools, dataLimit);
+
 }
 
 const displayAllAi = (tools , dataLimit) => {
@@ -25,7 +26,15 @@ const displayAllAi = (tools , dataLimit) => {
         const name = tool.name
         const image = tool.image
         const publish = tool.published_in
-        const features =tool.features
+        const features = tool =>{
+            let list ='';
+            for (let i = 0; i < tool.features.length; i++) {
+                 list += `<li>${tool.features[i]}</li>`;
+                
+            }
+            return list;
+        }
+        const id =tool.id
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
@@ -34,9 +43,7 @@ const displayAllAi = (tools , dataLimit) => {
                         <div class="card-body">
                             <h5 class="card-title">Features</h5>
                             <ol>
-                            <li class=" text-muted">${features[0]}</li>
-                            <li class=" text-muted">${features[1]}</li>
-                            <li class=" text-muted">${features[2]}</li> 
+                            ${features(tool)} 
                             </ol>
                         </div>
                         <hr class="">
@@ -46,7 +53,7 @@ const displayAllAi = (tools , dataLimit) => {
                        <small class="text-muted"><i class="far fa-calendar-alt"></i> <span> ${publish} </span> </small>
                      </div>
                      <div>
-                     <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#aiDetailModal">
+                     <button onclick=loadAiDetail('${id}') type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#aiDetailModal">
                      <i class="fas fa-arrow-right"></i>
                      </button>
                      </div>                     
@@ -70,9 +77,9 @@ const toggleLoader = isLoading=>{
 document.getElementById('show-all-btn').addEventListener('click',function(){
     const cardField = document.getElementById('ai-cards')
     cardField.innerHTML=""
+    processData();
     const backSection = document.getElementById('back-section')
     backSection.classList.remove('d-none')
-    processData();
 })
 
 document.getElementById('back-btn').addEventListener('click',function(){
@@ -85,5 +92,75 @@ const processData=(dataLimit)=>{
     toggleLoader(true)
     loadAllAi(dataLimit)
 }
+
+const loadAiDetail = async id =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayDetail(data.data)
+}
+
+const displayDetail = data =>{
+    console.log(data)
+    const name = data.name
+    const description = data.description
+    const image = data.image_link[0]
+    const basic = data.pricing
+    const pro = data.pricing
+    const enterprise = data.pricing
+    const integrations = data =>{
+        let li = ''
+        for (let i = 0; i < data.integrations.length; i++) {
+            li += `<li>${data.integrations[i]}</li>`;
+            
+        }
+        return li;
+    }
+    const features = data =>{
+        let li = ''
+        const feature = data.features
+        const keyArray = Object.keys(feature)
+        console.log(keyArray.length)
+        for (let i = 1; i < keyArray.length+1; i++) {
+            li += `<li>${feature[`${i}`].feature_name}</li>`
+            
+        }
+
+        return li;
+    }
+
+    const div = document.getElementById('modal-container')
+    div.innerHTML=`
+            <div class="row row-cols-2 g-5 p-4">
+            <div class="col bg-danger-subtle p-3 rounded">
+                <h5 class="w-75">${description ? description : 'No description found'}</h5>
+                <div class="d-flex gap-3 my-5">
+                    <div class="border rounded p-2 fw-semibold w-25 bg-secondary-subtle text-center text-success">${basic ? basic[0].price : 'Free of cost'} Basic</div>
+                    <div class="border rounded p-2 fw-semibold w-25 bg-secondary-subtle text-center text-warning">${pro ? pro[1].price : 'Free of cost'} pro </div>
+                    <div class="border rounded p-2 fw-semibold w-25 bg-secondary-subtle text-center text-danger">${enterprise ? enterprise[2].price : 'Free of cost'} Enterprise</div>
+                </div>
+                <div class="d-flex justify-content-around">
+                    <div>
+                        <h5>Features</h5>
+                        <ul>
+                        ${data.features? features(data): 'No Features Found'}
+                        </ul>
+                    </div>
+                
+                    <div>
+                        <h5>Integrations</h5>
+                        <ul>
+                        ${data.integrations? integrations(data) : 'No Integrations Found'}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+            <img class="img-fluid" src="${image}">
+            </div>
+    
+    `
+
+}
+
 processData(6);
-// loadAllAi();
